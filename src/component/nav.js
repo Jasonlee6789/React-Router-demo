faffc4cf4793a69d4690e0ad3e31a23a0d911711
Jasonlee6789 <jeffersonlee@163.com>
2022-03-29 14:07:59 +0900

    练习

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React from "react";
 
 export default function Nav() {
   return (
@@ -7,7 +7,7 @@ export default function Nav() {
       <div className="wrap">
         <h1 id="logo">KaiKeBa</h1>
         <nav className="nav">
-          <Link to="/">首页</Link>
+          <Link to="/">首页-日进练习李靖</Link>
 
           <Link to="/about">关于我们</Link>
 
